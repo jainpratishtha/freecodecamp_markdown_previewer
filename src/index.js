d39820733b3fdd3b9263e950ebd8b86500cab66c
jainpratishtha <49338769+jainpratishtha@users.noreply.github.com>
2020-08-09 17:13:15 +0530

    commit III

--- src/index.js
+++ src/index.js
@@ -1,25 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import marked from "marked";
 
+const initalState = `
+# Headin 1
+## Heading 2
+> Blockquotes are very handy in email to emulate reply text.
+> This line is part of the same quote.
+- list item 1
+- list item 2
+- list item 3
+Inline-style: 
+![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
 
-class App extends React.Component{
-  state={
-    keys:['Q','W','E','A','S','D','Z','X','C']
+hello \`<div></div>\`
+
+\`\`\`
+x=2+3+4;
+y=2+3+4;
+\`\`\`
+
+**asterisks** or __underscores__
+
+[visit my website](https://www.youtube.com/watch?v=CJt7uZD_iK0)
+`;
+class App extends React.Component {
+  state = {
+    text: initalState
+  };
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value
+    });
+  };
+  render() {
+    const { text } = this.state;
+    const markdown = marked(text, { breaks: true });
+    return (
+      <div>
+        <row>
+          <textarea id="editor" value={text} onChange={this.handleChange} />
+        </row>
+        <br />
+        <br />
+        <row>
+          <div id="preview" dangerouslySetInnerHTML={{ __html: markdown }} />
+        </row>
+      </div>
+    );
   }
-  render(){
-    const { keys }= this.state;
-    return <div id="drum-machine">
-      <div id="display">
-    {keys.map((key,id)=>(
-        <Box text={key} key={id} /> 
-        ))}    
-    </div></div>
-  }
-  
 }
- 
-const Box=(props)=>(
-  <div className="box">{props.text}</div>
-);
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
